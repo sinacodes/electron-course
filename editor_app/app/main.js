dd63defb2bbe949e97662b4763f9d317e9d791ec
Sina <sina.burow@gmx.de>
2020-04-15 23:02:26 +0200

    we did a lot of things

--- editor_app/app/main.js
+++ editor_app/app/main.js
@@ -17,7 +17,7 @@ app.on('ready', () => {
 });
 
 exports.getFileFromUser = () => {
-    const files = dialog.showOpenDialog({
+    const files = dialog.showOpenDialog(mainWindow, {
         properties: ['openFile'],
         filters: [
             {
@@ -38,9 +38,49 @@ exports.getFileFromUser = () => {
     openFile(file);
 };
 
-const openFile = (file) => {
+exports.saveMarkdown = (file, content) => {
+    if (!file) {
+        file = dialog.showSaveDialog(mainWindow, {
+            title: 'Save Markdown',
+            defaultPath: app.getPath('desktop'),
+            filters: [
+                {
+                    name: 'Markdown Files', 
+                    extentions: ['md', 'mdown', 'markdown']
+                }
+            ]
+        })
+    }
+
+    if (!file) return;
+
+    fs.writeFileSync(file, content);
+    // this makes sure if the file is just created to have file system file and app file in sync
+    openFile(file);
+}
+
+exports.saveHtml = content => {
+    const file = dialog.showSaveDialog(mainWindow, {
+        title: 'Save HTML',
+        defaultPath: app.getPath('desktop'),
+        filters: [
+            {
+                name: 'HTML Files',
+                extentions: ['html', 'htm']
+            }
+        ]
+    })
+
+    if (!file) return;
+
+    fs.writeFileSync(file, content);
+}
+
+const openFile = (exports.openFile = (file) => {
     // you get a Buffer that needs to be converted to a string
     const content = fs.readFileSync(file).toString();
-    console.log(content)
+
+    app.addRecentDocument(file);
+
     mainWindow.webContents.send('file-opened', file, content);
-}
+});
--- editor_app/app/renderer.js
+++ editor_app/app/renderer.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 const marked = require('marked');
 const { remote, ipcRenderer } = require('electron');
 
 const mainProcess = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
+
+let filePath = null;
+let originalContent = '';
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -17,17 +23,95 @@ const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = isEdited => {
+    let title = 'editor';
+
+    if (filePath) {
+        title = `${path.basename(filePath)} – ${title}`
+    }
+
+    if (isEdited) {
+        title = `${title} - (edited)`
+    }
+
+    // mac: make it more native
+    if (filePath) currentWindow.setRepresentedFilename(filePath);
+    currentWindow.setDocumentEdited(isEdited)
+
+    showFileButton.disabled = !filePath;
+    openInDefaultButton.disabled = !filePath;
+    saveMarkdownButton.disabled = !isEdited;
+    revertButton.disabled = !isEdited;
+
+    currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+
+  updateUserInterface(currentContent !== originalContent);
 });
 
 openFileButton.addEventListener('click', () => {
     mainProcess.getFileFromUser()
-})
+});
+
+saveMarkdownButton.addEventListener('click', () =>{
+    mainProcess.saveMarkdown(filePath, markdownView.value);
+});
+
+saveHtmlButton.addEventListener('click', () =>{
+    mainProcess.saveHtml(htmlView.innerHTML);
+});
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+    filePath = file;
+    originalContent = content;
+
     markdownView.value = content;
     renderMarkdownToHtml(content);
-    console.log(content)
+
+    updateUserInterface(false);
+})
+
+/********* DRAG AND DROP *************/  
+document.addEventListener('dragstart', event => event.preventDefault())
+document.addEventListener('dragover', event => event.preventDefault())
+document.addEventListener('dragleave', event => event.preventDefault())
+document.addEventListener('drop', event => event.preventDefault())
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const fileTypeIsSupported = (file) => {
+    return ['text/plain', 'text/markdown'].includes(file.type)
+};
+
+markdownView.addEventListener('dragover', event => {
+    const file = getDraggedFile(event);
+
+    if (fileTypeIsSupported(file)) {
+        markdownView.classList.add('drag-over');
+    } else {
+        markdownView.classList.add('drag-error');
+    }
+});
+
+markdownView.addEventListener('dragleave', event => {
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
+})
+
+markdownView.addEventListener('drop', event => {
+    const file = getDroppedFile(event);
+
+    if (fileTypeIsSupported(file)) {
+        mainProcess.openFile(file.path);
+    } else {
+       alert('That file type is not supported')
+    }
+
+    markdownView.classList.remove('drag-over');
+    markdownView.classList.remove('drag-error');
 })
